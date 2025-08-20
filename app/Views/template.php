a289205c72aaeb875a405b88aa2676e01422963d
amaneslera <amaneslera1@gmail.com>
2025-08-20 08:25:18 +0800

    Created navigation

--- app/Views/template.php
+++ app/Views/template.php
@@ -2,29 +2,44 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>My CodeIgniter 4 App</title>
+    <title>My App</title>
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+   
 </head>
 <body>
 
     
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">ITE311</a>
-            
+            <a class="navbar-brand" href="#">MY APP</a>
+            <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label32="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav"> -->
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
+    
+    
+    
 
-       <div class="alert alert-success" role="alert">
-            Bootstrap is ADDED! You can now use Bootstrap components!!!!!!
-       </div>
 
     
-    <div class="container mt-4">
-        <?= $this->renderSection('content') ?>
-    </div>
+    
 
 </body>
 </html>
