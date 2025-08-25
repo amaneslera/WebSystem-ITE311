cd940f6d4c7f2910ed6c9cb59c317166ee708090
unknown <amaneslera1@gmail.com>
2025-08-25 13:53:01 +0800

    Added Home controller, routes, and views for basic navigation

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Controllers\Home;
+use CodeIgniter\Honeypot\Exceptions\HoneypotException;
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/about', 'Home::about' );
+$routes->get('/contact', 'Home::contact' );
+
---
+++ app/Views/contact.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light " style="background-color: lavender;">
+        <div class="container-fluid">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="/about">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="/contact">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
+    
+    <h1>Contact Us</h1>
+    <p>You can contact us Here!</p>
+     
+</body>
+</html>
